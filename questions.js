341d71b446570a4f07f8f47f4db43bc400318d18
José Paulino <j.paulino1@hotmail.com>
2023-05-15 23:41:01 -0300

    questions js

---
+++ questions.js
@@ -0,0 +1,92 @@
+let questions = [
+  {
+    question: 'O que você gosta de comer?',
+    options:[
+      'What you like to eat?',
+      'What does you like to eat?',  
+      'What do you like to eat?'
+    ],
+    answer:2
+  },
+  {
+    question: 'Onde Ana estuda inglês?',
+    options:[
+      'Where do Ana study English?',
+      'Where does Ana studies English?',  
+      'Where does Ana study English?'
+    ],
+    answer:2
+  },
+  {
+    question: 'Quando nós temos que voltar?',
+    options:[
+      'When do we have to return?',
+      'When we have to return?',  
+      'When does we has to return?'
+    ],
+    answer:0
+  },
+  {
+    question: 'Por que você assiste anime?',
+    options:[
+      'Why you watch anime?',
+      'Why do you watch anime?',  
+      'Why do you watches anime?'
+    ],
+    answer:1
+  },
+  {
+    question: 'Quem sabe meu nome?',
+    options:[
+      'Who knows my name?',
+      'Who do knows my name?',  
+      'Who does know my name?'
+    ],
+    answer:0
+  },
+  {
+    question: '__________ does Andrew do for a living?',
+    options:[
+      'Where',
+      'What',     
+      'When'
+    ],
+    answer:1
+  },
+  {
+    question: '__________ do you study english?',
+    options:[
+      'Who',
+      'What',     
+      'Where'
+    ],
+    answer:2
+  },
+  {
+    question: '__________ does Katherine have so many cats?',
+    options:[
+      'Why',
+      'How many',     
+      'When'
+    ],
+    answer:0
+  },
+  {
+    question: '__________ does have problem with this subject?',
+    options:[
+      'What',
+      'Who',     
+      'Why'
+    ],
+    answer:1
+  },
+  {
+    question: '__________ do you want your coffee, ma`am?`',
+    options:[
+      'How',     
+      'What',
+      'How much'
+    ],
+    answer:0
+  },  
+]
